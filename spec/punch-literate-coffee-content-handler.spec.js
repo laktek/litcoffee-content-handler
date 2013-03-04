b2a22a332122bb6182c53e67c5e870c12db1bae9
Lakshan Perera <lakshan@web2media.net>
2013-03-04 16:43:13 +0800

    basic implementation

---
+++ spec/punch-literate-coffee-content-handler.spec.js
@@ -0,0 +1,12 @@
+var punch_literate_coffee_content_handler = require('../lib/punch-literate-coffee-content-handler.js');
+
+/* To learn more details on how
+ * to write Jasmine specs, visit:
+ * http://pivotal.github.com/jasmine/
+ */
+
+describe("calling the plugin", function() {
+
+	// Define the plugin specs
+
+});
